7d532314fa0e170a083b1422b595fe31c5a0ffb0
Suraj Narayanan S <surajx@gmail.com>
2015-02-09 13:39:53 +0530

    pointing to latest private coursera repo

--- Stanford-Part1/code/quick_sort.py
+++ Stanford-Part1/code/quick_sort.py
@@ -1,14 +1,26 @@
 def quick_sort(elements):
-  if len(elements)==1:
+  if len(elements)==1 or not elements:
     return elements
   pivot_idx = choosePivot(elements)
   (partitioned_elements, updated_pivot_idx) = partition(elements, pivot_idx)
   sorted_left_partition = quick_sort(partitioned_elements[:updated_pivot_idx])
+  pivot_ele = partitioned_elements[updated_pivot_idx]
+  del partitioned_elements[updated_pivot_idx]
   sorted_right_partition = quick_sort(partitioned_elements[updated_pivot_idx:])
-  return sorted_left_partition + sorted_right_partition
+  return sorted_left_partition + [pivot_ele] + sorted_right_partition
 
 def choosePivot(elements):
-  return elements[0]
+  return 0
 
 def partition(elements, pivot_idx):
-  pass
+  pivot_ele = elements[pivot_idx]
+  del elements[pivot_idx]
+  pivot_pos = 0
+  for elem_idx in range(0,len(elements)):
+    if elements[elem_idx]<pivot_ele:
+      elements[elem_idx], elements[pivot_pos] = elements[pivot_pos], elements[elem_idx]
+      pivot_pos+=1
+  elements.insert(pivot_pos, pivot_ele)
+  return (elements, pivot_pos)
+
+print quick_sort([9,3,8,2,5,1,4,7,6])
