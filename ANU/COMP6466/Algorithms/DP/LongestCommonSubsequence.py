27582eb24b839923285cb7727ab8b960471e606e
Suraj Narayanan S <surajx@gmail.com>
2015-08-26 22:47:14 +1000

    Longest Common Subsequence

--- ANU/COMP6466/Algorithms/DP/LongestCommonSubsequence.py
+++ ANU/COMP6466/Algorithms/DP/LongestCommonSubsequence.py
@@ -1,10 +1,29 @@
-def LCS(x,y):
+def LCS(X,Y):
   """
   Computes the length of the Longest common subsequence in x and y
-  Returns Int"""
+  Returns []"""
   #1. Identify the optimal substructure
-    #1.1 When calculating the Longest common subsequence we can see that if a prefix
-      # of X, X_i=<X1 X2 X3...Xi> is a common subsequence then
-  #2. Identify overlapping sub-problems
-  #3. Porpose a theorem for the given problem
-  #4. Obtain the final solution by
+    #1.1 When calculating the Longest Common Subsequence (LCS) we first assume that,
+      # length of LCS of prefixes of X_n & Y_m, namely, X_i=<x1x2x3...x_i> and
+      # Y_j=<y1,y2,y3....y_j> is L[i,j]. If x_i == y_j then L[i,j] = 1 + L[i-1,j-1]
+      # because the last character is a part of LCS[X_i, Y_j]. Now if x_i != y_j
+      # then the x_i is part of LCS[X_i, Y_j-1] or y_j is part of the LCS[X_i-1, y_j]
+  n = len(X)
+  m = len(Y)
+  L = []
+  for i in range(n+1):
+    L.append([0]*(m+1))
+  for i in range(n+1):
+    for j in range(m+1):
+      if i==0 or j==0: continue
+      if X[i-1]==Y[j-1]:
+        L[i][j] = 1 + L[i-1][j-1]
+      else:
+        L[i][j] = max(L[i-1][j], L[i][j-1])
+  print ''.join([str(y)+'\n' for y in L])
+  return L[n][m]
+
+if __name__ == '__main__':
+  print LCS("ABCDGH","AEDFHR")
+  print LCS("AGGTAB","GXTXAYB")
+
